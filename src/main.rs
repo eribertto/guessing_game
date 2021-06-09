330329cd8ca123913b32a679ca419c26c18da313
Eri Mendz <erimendz@gmail.com>
2021-06-09 13:43:07 +0300

    add commits completed so far

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
-    println!("Guess the number!");
+    println!("Welcome to the Guess-the-number game!");
 	let secret_number = rand::thread_rng().gen_range(1, 101);
 	//println!("The secret number is: {}", secret_number);
 	loop {
