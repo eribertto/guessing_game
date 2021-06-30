809e5a9ded08d853607ec229570a1af453c41aff
eribertt_fedorabox <erimendz@gmail.com>
2021-06-30 14:53:21 +0300

    change the prompt to something explicitly clear

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn main() {
 	let secret_number = rand::thread_rng().gen_range(1, 101);
 	//println!("The secret number is: {}", secret_number);
 	loop {
-		println!("Please input your guess.");
+		println!("Please input your guess between 1 and 100: ");
 		let mut guess = String::new();
 		io::stdin().read_line(&mut guess).expect("Failed to read line");
 		//let guess: u32 = guess.trim().parse().expect("Please enter a number!");
